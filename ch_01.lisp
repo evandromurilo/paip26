29388a028d6ca45ac9079c4477019e0640b1d9b5
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-26 14:16:21 -0400

    add power

--- ch_01.lisp
+++ ch_01.lisp
@@ -1,6 +1,7 @@
 (defparameter *titles* '(jr md dr mr mrs miss ms madam admiral major general))
 
 (defun last-name (name)
+  "Select the last name from a name represented as a list. (exercise 1.1)"
   (if (null name)
       nil
       (let ((last (last-name (rest name))))
@@ -9,3 +10,11 @@
 		nil
 		(first name))
 	    last))))
+
+(defun power (x n)
+  "Power raises x to the nth power. N must be an integer >= 0. (exercise 1.2)"
+  (if (zerop x)
+      1
+      (* x (power x (- n 1)))))
+
+
