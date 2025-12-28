9c011537a9ecc4101f9465fb30ea919802aad255
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-28 10:20:52 -0400

    handle flags (* +) on the generator

--- ch_02.lisp
+++ ch_02.lisp
@@ -2,6 +2,8 @@
 
 (defparameter *verbs* '(hit took saw liked killed massaged hugged shutted))
 (defparameter *nouns* '(woman ball table man tree pencil gun bathtub))
+(defparameter *adjectives* '(big little blue green huge funny smart crazy magnificent red))
+(defparameter *prepositions* '(to in by with on over under))
 (defparameter *articles* '(the a))
 
 (defun make-generator (wordlist)
@@ -22,14 +24,44 @@
 (add-generator 'verb (make-generator *verbs*))
 (add-generator 'noun (make-generator *nouns*))
 (add-generator 'article (make-generator *articles*))
-(add-generator 'noun-phrase (make-composite-generator '(article noun)))
+(add-generator 'adj (make-generator *adjectives*))
+(add-generator 'prep (make-generator *prepositions*))
+(add-generator 'pp (make-composite-generator '(prep noun-phrase)))
+(add-generator 'noun-phrase (make-composite-generator '(article adj* noun pp*)))
 (add-generator 'verb-phrase (make-composite-generator '(verb noun-phrase)))
 (add-generator 'sentence (make-composite-generator '(noun-phrase verb-phrase)))
+
+(defun butlast-str (what)
+  "Returns the string separated of the last character."
+  (values (subseq what 0 (- (length what) 1))
+	  (char what (- (length what) 1))))
+
+(defun parse-gen-name (what)
+  "Parse a generator name to see if there are any flags, like * (zero-or-many) or + (one-or-many)."
+  (multiple-value-bind (start end) (butlast-str (string what))
+    (if (or (equal end #\*) (equal end #\+))
+	(values (intern start) end)
+	(values what nil))))
+
+(defun zero-or-many (fun)
+  "Half the time returns nil, the other half returns the result of calling the function one-or-many times. Function must return a list."
+  (if (= (random 2) 0)
+      nil
+      (one-or-many fun)))
+
+(defun one-or-many (fun)
+  "Returns a list with the value returned by function, one or more times. Function must return a list."
+  (append (funcall fun)
+	  (zero-or-many fun)))
 	       
 (defun generate (what)
   "Generate a sentence based on a list or symbol."
   (cond ((null what) nil)
-	((atom what) (funcall (gethash what *generators*)))
+	((atom what)
+	 (multiple-value-bind (what flag) (parse-gen-name what)
+	   (cond ((equal flag #\*) (zero-or-many (gethash what *generators*)))
+		 ((equal flag #\+) (one-or-many (gethash what *generators*)))
+		 (t (funcall (gethash what *generators*))))))
 	(t (append (generate (first what))
 		   (generate (rest what))))))
 
