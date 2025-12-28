9019b341baaf103847ff06a43acae2915a679eb3
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-28 09:52:30 -0400

    add straighforward solution from book

--- ch_02.lisp
+++ ch_02.lisp
@@ -32,5 +32,22 @@
 	((atom what) (funcall (gethash what *generators*)))
 	(t (append (generate (first what))
 		   (generate (rest what))))))
+
+;; follow up with the book
+
+(defun sentence ()    (append (noun-phrase) (verb-phrase)))
+(defun noun-phrase () (append (Article) (Noun)))
+(defun verb-phrase () (append (Verb) (noun-phrase)))
+(defun Article ()     (one-of '(the a)))
+(defun Noun ()        (one-of '(man ball woman table)))
+(defun Verb ()        (one-of '(hit took saw liked)))
+
+(defun one-of (set)
+  "Pick one element of set, and make a list of it."
+  (list (random-elt set)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random."
+  (elt choices (random (length choices))))
 			     
   
