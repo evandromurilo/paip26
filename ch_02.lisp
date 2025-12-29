3c19b7b086272cf9f8399bf1946ac19d6bd68b70
Evandro Murilo <evandromurilob@hotmail.com>
2025-12-29 13:02:03 -0400

    wrap up chapter 2

--- ch_02.lisp
+++ ch_02.lisp
@@ -143,6 +143,21 @@
   '((noun -> verb noun))
   "This grammar never generates 'noun.")
 
+(defparameter *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+
 (defvar *grammar* *simple-grammar*
   "The grammar used by generate. Initially, this is *simple-grammar*, but we can switch to other grammars.")
 
@@ -169,6 +184,32 @@
 	((rewrites phrase)
 	 (generate (random-elt (rewrites phrase))))
 	(t (list phrase))))
+
+(defun generate-tree (phrase)
+  "Generate a random sentence or phrase, with a complete parse tree."
+  (cond ((listp phrase)
+	 (mapcar #'generate-tree phrase))
+	((rewrites phrase)
+	 (cons phrase (generate-tree (random-elt (rewrites phrase)))))
+	(t (list phrase))))
+
+(defun generate-all (phrase)
+  "Generate a list of all possible expansions of this phrase."
+  (cond ((null phrase) (list nil))
+	((listp phrase)
+	 (combine-all (generate-all (first phrase))
+		      (generate-all (rest phrase))))
+	((rewrites phrase)
+	 (mappend #'generate-all (rewrites phrase)))
+	(t (list (list phrase)))))
+
+(defun combine-all (xlist ylist)
+  "Return a list of lists formed by appending a y to an x.
+  E.g., (combine-all '((a) (b)) '((1) (2)))
+  -> ((A 1) (B 1) (A 2) (B 2))"
+  (mappend #'(lambda (y)
+	       (mapcar #'(lambda (x) (append x y)) xlist))
+	   ylist))	 
 	
 ;; exercises for section 2.3
 
@@ -192,7 +233,15 @@
 (defun terminalp (term)
   "Check if term has no rewrites in the grammar, and is therefore terminal."
   (null (rewrites term)))
-		 
-	 
-  
 
+;; exercises for section 2.7
+
+(defun cross-product (fun xlist ylist)
+  "Apply fun to every combination of x and y. (exercise 2.4)"
+  (mappend #'(lambda (y)
+	       (mapcar #'(lambda (x) (funcall fun x y)) xlist))
+	   ylist))
+
+(defun combine-all (xlist ylist)
+  "Version of combine-all in terms of cross-product. (exercise 2.4)"
+  (cross-product #'append xlist ylist))
