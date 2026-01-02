7f7b1499ce1eb162c78fe05cc78ee62db638a4c8
Evandro Murilo <evandromurilob@hotmail.com>
2026-01-02 14:07:27 -0400

    print-dotted and print-list

--- ch_03.lisp
+++ ch_03.lisp
@@ -4,3 +4,33 @@
  (let* ((x 6)
 	(y (* x x)))
    (+ x y)))
+
+(defun print-dotted (exp)
+  "Print an expression in dotted pair notation. (exercise 3.3)"
+  (if (consp exp)
+      (progn
+	(princ "(")
+	(print-dotted (car exp))
+	(princ " . ")
+	(print-dotted (cdr exp))
+	(princ ")"))
+      (princ exp))
+  nil)
+
+(defun print-list (exp &optional (is-head t))
+  "Print an expression in dotthed pair notation when necessary, but otherwise use normal list notation. (exercise 3.4)"
+  (if (consp exp)
+      (progn
+	(when is-head
+	  (princ "("))
+	(print-list (car exp))
+	(cond ((null (cdr exp))
+	       (princ ")"))
+	      ((listp (cdr exp))
+	       (princ " ")
+	       (print-list (cdr exp) nil))
+	      (t
+	       (princ " . ")
+	       (princ (cdr exp))
+	       (princ ")"))))
+      (princ exp)))
