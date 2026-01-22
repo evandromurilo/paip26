5d0f92822b03de02764dac2ac86d9aa28dd24563
Evandro Murilo <evandromurilob@hotmail.com>
2026-01-22 19:11:18 -0400

    banana-ops

--- ch_04_gps.lisp
+++ ch_04_gps.lisp
@@ -239,3 +239,31 @@
 (defun use (oplist)
   "Use oplist as the default list of operators."
   (length (setf *ops* oplist)))
+
+(defparameter *banana-ops*
+  (list
+   (op 'climb-on-chair
+       :preconds '(chair-at-middle-room at-middle-room on-floor)
+       :add-list '(at-bananas on-chair)
+       :del-list '(at-middle-room on-floor))
+   (op 'push-chair-from-door-to-middle-room
+       :preconds '(chair-at-door at-door)
+       :add-list '(chair-at-middle-room at-middle-room)
+       :del-list '(chair-at-door at-door))
+   (op 'walk-from-door-to-middle-room
+       :preconds '(at-door on-floor)
+       :add-list '(at-middle-room)
+       :del-list '(at-door))
+   (op 'grasp-bananas
+       :preconds '(at-bananas empty-handed)
+       :add-list '(has-bananas)
+       :del-list '(empty-handed))
+   (op 'drop-ball
+       :preconds '(has-ball)
+       :add-list '(empty-handed)
+       :del-list '(has-ball))
+   (op 'eat-bananas
+       :preconds '(has-bananas)
+       :add-list '(empty-handed not-hungry)
+       :del-list '(has-bananas hungry))))
+       
