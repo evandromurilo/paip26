91d2d2ef6d0295814f8ab7e3c8483843b19846d4
Evandro Murilo <evandromurilob@hotmail.com>
2026-03-08 16:39:03 -0400

    complete the binding abstraction over assoc

--- ch_05_eliza.lisp
+++ ch_05_eliza.lisp
@@ -3,28 +3,43 @@
 
 (in-package :eliza)
 
-(defun pat-match (pat input &optional (matches nil))
+(defun pat-match (pat input &optional (bindings nil))
+  "Does pattern match innput?"
   (if (and (null pat) (null input))
-      (values t matches)
+      (values t bindings)
       (if (variable-p pat)
-	  (let ((prev (assoc pat matches)))
+	  (let ((prev (lookup pat bindings)))
 	    (if prev
-		(if (equal (cdr prev) input)
+		(if (equal prev input)
 		    (values t nil)
 		    (values nil nil))
-		(values t (list (make-match pat input)))))
+		(values t (list (make-binding pat input)))))
 	  (if (atom pat)
 	      (if (equal pat input)
 		  (values t nil)
 		  (values nil nil))
-	      (multiple-value-bind (success new-matches) (pat-match (first pat) (first input) matches)
+	      (multiple-value-bind (success new-bindings) (pat-match (first pat) (first input) bindings)
 		(if success
-		    (pat-match (rest pat) (rest input) (append new-matches matches))
+		    (pat-match (rest pat) (rest input) (append new-bindings bindings))
 		    (values nil nil)))))))
 
 (defun variable-p (thing)
+  "Is x a variable (a symbol beginning with '?')?"
   (and (symbolp thing)
        (equal #\? (aref (string thing) 0))))
 
-(defun make-match (variable match)
-  (cons variable match))
+(defun get-binding (var bindings)
+  "Find a (variable . value) pair in a binding list."
+  (assoc var bindings))
+
+(defun binding-val (binding)
+  "Get the value part of a single binding."
+  (cdr binding))
+
+(defun lookup (var bindings)
+  "Get the value part (for var) from a binding list."
+  (binding-val (get-binding var bindings)))
+
+(defun make-binding (var val)
+  "Make a new (var . value) pair for a binding list."
+  (cons var val))
