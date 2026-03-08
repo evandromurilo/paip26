c5b46c9d9c8168893a3b75cc781ac5aecf0af691
Evandro Murilo <evandromurilob@hotmail.com>
2026-03-08 16:27:59 -0400

    do not double match on pat-match

--- ch_05_eliza.lisp
+++ ch_05_eliza.lisp
@@ -7,18 +7,23 @@
   (if (and (null pat) (null input))
       (values t matches)
       (if (variable-p pat)
-	  (values t (list (make-match pat input)))
+	  (let ((prev (assoc pat matches)))
+	    (if prev
+		(if (equal (cdr prev) input)
+		    (values t nil)
+		    (values nil nil))
+		(values t (list (make-match pat input)))))
 	  (if (atom pat)
 	      (if (equal pat input)
 		  (values t nil)
 		  (values nil nil))
-	      (multiple-value-bind (success new-matches) (pat-match (first pat) (first input))
+	      (multiple-value-bind (success new-matches) (pat-match (first pat) (first input) matches)
 		(if success
 		    (pat-match (rest pat) (rest input) (append new-matches matches))
 		    (values nil nil)))))))
 
 (defun variable-p (thing)
-  (and (atom thing)
+  (and (symbolp thing)
        (equal #\? (aref (string thing) 0))))
 
 (defun make-match (variable match)
